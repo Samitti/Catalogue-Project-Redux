4c60ad6b65aaf6ecb20686f6358e4bfaca608fa4
Samitti <samuelteweldebrhan@gmail.com>
2021-02-01 18:42:09 +0300

    connect API for game list

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
 import GetGameList from '../actions/gameActions';
 
 const GameList = () => {
   const dispatch = useDispatch();
-  const gameList = useSelector(state => state.GameList);
+  const gameList = useSelector(state => state.GameList.data);
 
   React.useEffect(() => {
     if (gameList.length === 0) {
@@ -13,9 +14,21 @@ const GameList = () => {
     }
   }, [dispatch]);
 
+  const gameElements = gameList.map(game => (
+    <article key={game.id} className="gameItem">
+      <Link to={`./game/${game.id}`} game={game}>
+        <img className="gameitemImg" src={game.thumbnail} alt={game.id} />
+      </Link>
+    </article>
+  ));
+
   const showData = () => {
-    if (!_.isEmpty(gameList.data)) {
-      return <p>Have Data</p>;
+    if (!_.isEmpty(gameList)) {
+      return (
+        <div className="gameListContainer">
+          {gameElements}
+        </div>
+      );
     }
 
     if (gameList.loading) {
