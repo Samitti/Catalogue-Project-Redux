8a24e7e4c678bc5b9857897a78d1e283bb3bbf83
Samitti <samuelteweldebrhan@gmail.com>
2021-02-01 11:05:22 +0300

    initial project setup

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,5 @@
-import {createStore} from "redux";
-import RootReducer from "./reducers/RootReducer";
+import { createStore } from 'redux';
+import RootReducer from './reducers/RootReducer';
 
 const Store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
